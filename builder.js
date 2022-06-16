4f855900c7056a581e1288aaa2209684108f9b51
Unzor <71789103+Unzor@users.noreply.github.com>
2022-06-16 15:04:34 -0500

    SPVM v1.0.4 - add installation of specific commits

--- builder.js
+++ builder.js
@@ -28,7 +28,9 @@ async function build_spwn() {
 			console.log('Building SPWN... (Step 3: building)')
 			spawnSync('cd spwn && cargo build --release', {shell: true, stdio: "inherit"});
 			console.log('Building SPWN... (Step 4: installing)')
-			fs.mkdirSync('C:\\Program Files\\spwn\\latest')
+			if (!fs.existsSync('C:\\Program Files\\spwn\\latest')) {
+				fs.mkdirSync('C:\\Program Files\\spwn\\latest')
+			}
 			fse.moveSync('spwn/libraries', 'C:\\Program Files\\spwn\\latest\\libraries')
 			fs.renameSync('spwn/target/release/spwn.exe', 'C:\\Program Files\\spwn\\latest\\spwn.exe')
 			fs.rmSync('spwn', {recursive: true});
@@ -42,4 +44,35 @@ async function build_spwn() {
 		- Visual Studio 2017 build tools (USE C++ BUILD TOOLS WITH WINDOWS 10 SDK: https://aka.ms/vs/17/latest/vs_BuildTools.exe)`)
 	}
 }
-module.exports = build_spwn
+
+async function build_spwn_from_commit(c) {
+	console.log('Building SPWN... (Step 1: Checking for Rust)')
+	const exists = await checkForRust();
+	if (exists && fs.existsSync('C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\SDK')) {
+		console.log('Building SPWN... (Step 2: downloading)')
+		const dir = path.join(process.cwd(), 'spwn')
+		git.clone({ fs, http, dir, url: 'https://github.com/Spu7Nix/SPWN-language', ref: c.split('commit-')[1] }).then(() => {
+			console.log('Building SPWN... (Step 3: building)')
+			spawnSync('cd spwn && cargo build --release', {shell: true, stdio: "inherit"});
+			console.log('Building SPWN... (Step 4: installing)')
+			if (!fs.existsSync('C:\\Program Files\\spwn\\'+c)) {
+				fs.mkdirSync('C:\\Program Files\\spwn\\'+c)
+			}
+			fse.moveSync('spwn/libraries', 'C:\\Program Files\\spwn\\'+c+'\\libraries')
+			fs.renameSync('spwn/target/release/spwn.exe', 'C:\\Program Files\\spwn\\'+c+'\\spwn.exe')
+			fs.rmSync('spwn', {recursive: true});
+			fs.writeFileSync('C:\\Program Files\\spwn\\'+c+'\\tags.txt', c)
+			console.log('SPWN commit '+c.split("-")[1]+' installed! Switch to this commit using "spvm use '+c+'".')
+		})
+	} else {
+		console.log(`Rust or Visual Studio Build tools are not installed, install Rust and/or Visual Studio 2017 build tools here: 
+		 (Visual Studio build tools necessary for building SPWN)
+		- Rust: https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe
+		- Visual Studio 2017 build tools (USE C++ BUILD TOOLS WITH WINDOWS 10 SDK: https://aka.ms/vs/17/latest/vs_BuildTools.exe)`)
+	}
+}
+
+module.exports = {
+	build_latest: build_spwn,
+	build_commit: build_spwn_from_commit
+}
